3aec80ba1d931e4187ce193b329bf7202fe65ec0
Subotai54 <subotai54@4261811f904c>
2019-11-29 23:12:56 +0000

    First commit

---
+++ mysite/main/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'main/index.html')
+
