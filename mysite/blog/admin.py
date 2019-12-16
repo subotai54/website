943cb66c5217d595edc8615d8102fe20ffa1a545
Subotai54 <samuel.bruflodt@gmail.com>
2019-12-16 04:41:33 +0000

    Added comments section

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
+    list_display = ('title', 'slug', 'status','created_on', 'tags')
+    list_filter = ("status",'tags')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-  
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-##    tags = models.CharField(maxlength=200)
+    tags = models.CharField(max_length=200, default="Subotai54")
 
     class Meta:
         ordering = ['-created_on']
@@ -27,3 +27,17 @@ class Post(models.Model):
 
     def get_tag_list(self):
         return re.split(" ", self.tags)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
