53ced8e771da7184b900dba6f2456edf64d0349c
Subotai54 <samuel.bruflodt@gmail.com>
2019-12-21 20:07:53 +0000

    Added form to allow users to add a movie to list, verified integrity against existing ranking or existing film.

--- mysite/rank/forms.py
+++ mysite/rank/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import createdRankings
+from .models import createdRankings, film
 
 class RankForm (forms.ModelForm):
-    
-    
+
+
     class Meta:
         model = createdRankings
         fields =['title','subtitle']
+
+class FilmForm (forms.ModelForm):
+
+
+    class Meta:
+        model = film
+        fields =['name','rank']
--- mysite/rank/views.py
+++ mysite/rank/views.py
@@ -1,13 +1,16 @@
 from django.views import generic
-from .models import createdRankings
+from .models import createdRankings, film
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.contrib import messages
-from .forms import RankForm
+from .forms import RankForm, FilmForm
 from django.utils import timezone
 from django.template.defaultfilters import slugify
+from django.db import models
+from django.db import IntegrityError
+from django.db.models import Max
 
 
 class ListList(LoginRequiredMixin, generic.ListView):
@@ -25,18 +28,37 @@ def bad_url(request,bad):
 
 @login_required
 def list_detail(request,author, slug):
-    List = get_object_or_404(createdRankings, slug=slug)
+    try:
+        List = get_object_or_404(createdRankings, slug=slug)
+        form = FilmForm()
+        if List.author == request.user:
 
-    if List.author == request.user:
+            template_name = 'rank/list_detail.html'
+            films = List.film.all()
+            if request.method == "POST":
+                form = FilmForm(request.POST)
+            if form.is_valid():
+                newFilm = form.save(commit=False)
+                newFilm.author = request.user
+                newFilm.created_on = timezone.now()
+                newFilm.parentRanking= List
+                highest = films.aggregate(Max('rank'))
+                max_int = int(highest['rank__max'])
+                if max_int == newFilm.rank-1:
+                    newFilm.save()
+                else:
+                    return render(request, 'rank/list_detail.html', {'form': form,'List': List,'films': films,"message": str(max_int)+str(newFilm.rank)})
 
-        template_name = 'rank/list_detail.html'
-        films = List.film.all()
 
+                return redirect('list_detail',author=request.user, slug=slug)
 
-        return render(request, template_name, {'List': List,
-                                               'films': films})
-    else:
-        raise PermissionDenied("This list does not belong to you.")
+            return render(request, 'rank/list_detail.html', {'form': form,'List': List,'films': films})
+
+        else:
+            raise PermissionDenied("This list does not belong to you.")
+    except IntegrityError as e:
+            return render(request, 'rank/list_detail.html', {'form': form,'List': List,'films': films,"message": "Rank or film already exists"})
+
 
 def rank_new(request):
     form = RankForm()
