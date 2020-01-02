31369e526a13540464bb67940f7217e1c4978a7f
Subotai54 <samuel.bruflodt@gmail.com>
2020-01-02 01:33:09 +0000

    fixed the area of the square in sketchsquare so that the square is the same size with any square count

--- mysite/static/projects/squaresketch.js
+++ mysite/static/projects/squaresketch.js
@@ -1,15 +1,34 @@
-function makeSquares(total = 4){
+function makeSquares(total = 64){
+    var h=500/total;
+    var w=500/total;
     for(var i =1; i<= total; i++){
-        $('#main_square').append($('<ul/>', { id: 'r' + i, 'class' : 'w-75 sketch-row list-inline  mx-0 my-0'}))
+        $('#main_square').append($('<ul/>', { id: 'r' + i, 'class' : 'sketch-row list-inline  mx-0 my-0'}))
 
+        $('#'+'r'+i).height(h)
         for(var j =1; j<= total; j++){
-            $("#"+'r'+i).append($('<div/>', { id: 'r' + i + j, 'class' : 'col-auto sketch list-inline-item border mr-0'}))
+            $("#"+'r'+i).append($('<div/>', { id: 'r' + i +'j'+ j, 'class' : 'sketch list-inline-item border mx-0'}))
             var newText = 'Sample';
-            var newName = 'r'+i+j;
-            $('#'+newName).text(newText);
+            var newName = 'r'+i+'j'+j;
+            $('#'+newName).width(w)
+            $('#'+newName).height(h)
         }
+
     }
+
+    $('#'+'r'+total).addClass("mb-5")
 }
 $(document).on("mouseenter",".sketch", function() {
-    $( this ).addClass('red');
+    var temp = $(this).css("background-color")
+    var rgb = temp.match(/[0-9].+/g);
+    var r = rgb[0].split(',')[0];
+    var g = rgb[0].split(',')[1];
+    var b = rgb[0].split(',')[2];
+    var c1 = parseInt(r)-25
+    var c2 = parseInt(g)-25
+    var c3 = parseInt(b)-25
+    $( this ).css("background-color","rgb("+c1+","+c2+","+c3+")");
 })
+
+
+
+
